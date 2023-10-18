be584c57d97c29104f060ca98c51e747763f6eaf
sjohnatas7 <johnatas@usp.br>
2023-10-18 13:43:59 -0300

    adicionar imgs e mudar galeria de produtos

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Obtém todas as imagens da galeria
     const imagens = document.querySelectorAll('.imagemQuadrada');
     console.log("JS ATIVADO")
     imagens.forEach(imagem => {
-        console.log(imagem)
-        imagem.addEventListener('click', function() {
+        imagem.addEventListener('click', function () {
             // Cria uma div para a imagem expandida
             const lightbox = document.createElement('div');
             lightbox.className = 'lightbox';
             // Cria a imagem expandida
             const imagemExpandida = document.createElement('img');
 
-            img = imagem.firstChild
+            const img = imagem.firstChild
             imagemExpandida.src = img.src;
 
             // Adiciona a imagem expandida à div do lightbox
@@ -19,11 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Adiciona o lightbox ao corpo do documento
             document.body.appendChild(lightbox);
-            console.log("FOI");
             // Fecha o lightbox quando clicado
-            lightbox.addEventListener('click', function() {
+            lightbox.addEventListener('click', function () {
                 document.body.removeChild(lightbox);
             });
         });
     });
-});
+    const carrosels = document.querySelector('.carosel-slide img');
+    const left = document.querySelector('.nav-left')
+    const right = document.querySelector('.nav-right')
+    right.addEventListener('click', ()=>{
+        const temp = carrosels[1].src
+        carrosels[1].src = carrosels[0].src
+        carrosels[0].src = temp
+    })
+    console.log(center);
+}); 
