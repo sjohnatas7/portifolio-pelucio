7836d75081c03d549ac289a4d6bdae79889ee0e5
sjohnatas7 <johnatas@usp.br>
2024-01-16 16:06:41 -0300

    Adicionando resposta para o formulario

--- index.js
+++ index.js
@@ -41,5 +41,34 @@ document.addEventListener("DOMContentLoaded", function () {
         carrosels[0].src = carrosels[1].src
         carrosels[1].src = temp
     })
-    console.log(center);
+    var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
 }); 
